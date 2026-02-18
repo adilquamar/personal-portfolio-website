42fbe799d0f435027a565d0ad24915833ec317e5
Adil Quamar <adil.quamar14@gmail.com>
2026-02-18 00:07:47 -0800

    fix: add dates for amazon summer internships

--- src/data/all-projects.ts
+++ src/data/all-projects.ts
@@ -3,7 +3,7 @@ import type { Project } from "./projects";
 export const allProjects: Project[] = [
   {
     id: "alexa-feedback-skill",
-    title: "SWE Intern — Amazon",
+    title: "SWE Intern — Amazon (Summer 2021)",
     description:
       "Created an internal Alexa Skill (1P) for feedback collection on Alexa's multimodal devices. Leveraged AWS services to host, monitor, and scale a containerized application on ECS/Fargate. Designed and developed RESTful APIs using Java. Streamlined feedback collection and visualization for internal teams.",
     technologies: [
@@ -17,7 +17,7 @@ export const allProjects: Project[] = [
   },
   {
     id: "alexa-content-freshness",
-    title: "SWE Intern — Amazon",
+    title: "SWE Intern — Amazon (Summer 2022)",
     description:
       "Designed and developed a system to guarantee content freshness on Alexa multimodal devices. Implemented cloud-based pub-sub architecture triggered by user-modified settings. Utilized AWS SNS and SQS for efficient message distribution. Tested and deployed containerized application running on ECS/Fargate. Successfully pushed code to production.",
     technologies: ["Java", "AWS SNS", "AWS SQS", "ECS/Fargate", "Docker"],
